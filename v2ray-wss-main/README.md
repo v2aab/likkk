554d40e40733b5c85a95ac0430557b100fa06c23
v2aab <120998178+v2aab@users.noreply.github.com>
2022-12-18 18:39:08 -0800

    Add files via upload

---
+++ v2ray-wss-main/README.md
@@ -0,0 +1,28 @@
+
+搭建 Shadowsocks-libev 和 V2ray+ Nginx + WebSocket 代理脚本，可以选择单独安装或一起安装，支持 Debian、Ubuntu、Centos，并支持甲骨文ARM平台。
+
+简单点讲，没域名的用户可以安装 Shadowsocks-libev 代理，有域名的可以安装 V2ray+ Nginx + WebSocket 代理，各取所需。
+
+WSS 代理，443和8080端口都是可用端口，8080 是免流端口，关闭 tls 后，只要在伪装域名里填上运营商免流链接就可以免流了。
+
+运行脚本：
+
+`wget https://git.io/tcp-wss.sh && bash tcp-wss.sh`
+
+便宜VPS推荐：https://1kb.day/deals.html
+
+![image](https://user-images.githubusercontent.com/13328328/127747290-d6485b45-f84f-44da-ad32-6d374f21d35f.JPG)
+
+已测试系统如下：
+
+Debian 9, 10, 11
+
+Ubuntu 16.04, 18.04, 20.04
+
+CentOS 7
+
+WS客户端代理信息保存在：
+`cat /usr/local/etc/v2ray/client.json`
+
+Shadowsocks客户端代理信息：
+`cat /etc/shadowsocks-libev/config.json`
---
+++ v2ray-wss-main/tcp-wss.sh
@@ -0,0 +1,284 @@
+#!/bin/sh
+# forum: https://1024.day
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
+timedatectl set-timezone Asia/Shanghai
+v2path=$(cat /dev/urandom | head -1 | md5sum | head -c 6)
+v2uuid=$(cat /proc/sys/kernel/random/uuid)
+
+install_precheck(){
+    echo "====输入已经DNS解析好的域名===="
+    read domain
+    
+    if [ -f "/usr/bin/apt-get" ]; then
+        apt-get update -y
+        apt-get install -y net-tools curl
+    else
+        yum update -y
+        yum install -y epel-release
+        yum install -y net-tools curl
+    fi
+
+    sleep 3
+    isPort=`netstat -ntlp| grep -E ':80 |:443 '`
+    if [ "$isPort" != "" ];then
+        clear
+        echo " ================================================== "
+        echo " 80或443端口被占用，请先释放端口再运行此脚本"
+        echo
+        echo " 端口占用信息如下："
+        echo $isPort
+        echo " ================================================== "
+        exit 1
+    fi
+}
+
+install_nginx(){
+    if [ -f "/usr/bin/apt-get" ];then
+        apt-get install -y nginx
+    else
+        yum install -y nginx
+    fi
+
+cat >/etc/nginx/nginx.conf<<EOF
+pid /var/run/nginx.pid;
+worker_processes auto;
+worker_rlimit_nofile 51200;
+events {
+    worker_connections 1024;
+    multi_accept on;
+    use epoll;
+}
+http {
+    server_tokens off;
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 120s;
+    keepalive_requests 10000;
+    types_hash_max_size 2048;
+    include /etc/nginx/mime.types;
+    access_log off;
+    error_log /dev/null;
+    server {
+        listen 80;
+        listen [::]:80;
+        server_name $domain;
+        location / {
+            return 301 https://\$server_name\$request_uri;
+        }
+    }
+    server {
+        listen 443 ssl http2;
+        listen [::]:443 ssl http2;
+        server_name $domain;
+        ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+        ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE;
+        ssl_prefer_server_ciphers on;
+        ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;        
+        location / {
+            default_type text/plain;
+            return 200 "Hello World !";
+        }        
+        location /$v2path {
+            proxy_redirect off;
+            proxy_pass http://127.0.0.1:8080;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$http_host;
+        }
+    }
+}
+EOF
+}
+
+acme_ssl(){    
+    apt-get -y install cron socat || yum -y install cronie socat
+    curl https://get.acme.sh | sh -s email=my@example.com
+    mkdir -p /etc/letsencrypt/live/$domain
+    ~/.acme.sh/acme.sh --issue -d $domain --standalone --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "~/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchain-file /etc/letsencrypt/live/$domain/fullchain.pem --key-file /etc/letsencrypt/live/$domain/privkey.pem --reloadcmd \"systemctl restart nginx\""
+}
+
+install_v2ray(){    
+    bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --version v4.45.2
+    
+cat >/usr/local/etc/v2ray/config.json<<EOF
+{
+  "inbounds": [
+    {
+      "port": 8080,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$v2uuid"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+        "path": "/$v2path"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
+
+    systemctl enable v2ray.service && systemctl restart v2ray.service
+    rm -f tcp-wss.sh install-release.sh
+
+cat >/usr/local/etc/v2ray/client.json<<EOF
+{
+===========配置参数=============
+地址：${domain}
+端口：443/8080
+UUID：${v2uuid}
+加密方式：aes-128-gcm
+传输协议：ws
+路径：/${v2path}
+底层传输：tls
+注意：8080是免流端口不需要打开tls
+}
+EOF
+
+    clear
+}
+
+install_sslibev(){
+    if [ -f "/usr/bin/apt-get" ];then
+        apt-get update -y
+        apt-get install -y --no-install-recommends \
+            autoconf automake debhelper pkg-config asciidoc xmlto libpcre3-dev apg pwgen rng-tools \
+            libev-dev libc-ares-dev dh-autoreconf libsodium-dev libmbedtls-dev git
+    else
+        yum update -y
+        yum install epel-release -y
+        yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto c-ares-devel libev-devel libsodium-devel mbedtls-devel git -y  
+    fi
+
+    git clone https://github.com/shadowsocks/shadowsocks-libev.git
+    cd shadowsocks-libev
+    git submodule update --init --recursive
+    ./autogen.sh && ./configure --prefix=/usr && make
+    make install
+    mkdir -p /etc/shadowsocks-libev
+
+cat >/etc/shadowsocks-libev/config.json<<EOF
+{
+    "server":["[::0]","0.0.0.0"],
+    "server_port":10240,
+    "password":"$v2uuid",
+    "timeout":600,
+    "method":"chacha20-ietf-poly1305"
+}
+EOF
+
+cat >/etc/systemd/system/shadowsocks.service<<EOF
+[Unit]
+Description=Shadowsocks Server
+After=network.target
+[Service]
+ExecStart=/usr/bin/ss-server -c /etc/shadowsocks-libev/config.json
+Restart=on-abort
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload && systemctl enable shadowsocks.service && systemctl restart shadowsocks.service
+    cd ..
+    rm -rf shadowsocks-libev tcp-wss.sh
+    clear
+}
+
+client_v2ray(){
+    echo
+    echo "安装已经完成"
+    echo
+    echo "===========v2ray配置参数============"
+    echo "地址：${domain}"
+    echo "端口：443/8080"
+    echo "UUID：${v2uuid}"
+    echo "加密方式：aes-128-gcm"
+    echo "传输协议：ws"
+    echo "路径：/${v2path}"
+    echo "底层传输：tls"
+    echo "注意：8080是免流端口不需要打开tls"
+    echo
+}
+
+client_sslibev(){
+    echo
+    echo "安装已经完成"
+    echo
+    echo "===========Shadowsocks配置参数============"
+    echo "地址：0.0.0.0"
+    echo "端口：10240"
+    echo "密码：${v2uuid}"
+    echo "加密方式：chacha20-ietf-poly1305"
+    echo "传输协议：tcp"
+    echo
+}
+
+start_menu(){
+    clear
+    echo " ================================================== "
+    echo " 论坛：https://1024.day                              "
+    echo " 介绍：一键安装Shadowsocks-libev和v2ray+ws+tls代理    "
+    echo " 系统：Ubuntu、Debian、CentOS                        "
+    echo " ================================================== "
+    echo
+    echo " 1. 安装Shadowsocks-libev"
+    echo " 2. 安装v2ray+ws+tls"
+    echo " 3. 同时安装上述两种代理"
+    echo " 0. 退出脚本"
+    echo
+    read -p "请输入数字:" num
+    case "$num" in
+    1)
+    install_sslibev
+    client_sslibev
+    ;;
+    2)
+    install_precheck
+    install_nginx
+    acme_ssl
+    install_v2ray
+    client_v2ray
+    ;;
+    3)
+    install_precheck
+    install_nginx
+    acme_ssl
+    install_v2ray
+    install_sslibev
+    client_v2ray
+    client_sslibev
+    ;;
+    0)
+    exit 1
+    ;;
+    *)
+    clear
+    echo "请输入正确数字"
+    sleep 2s
+    start_menu
+    ;;
+    esac
+}
+
+start_menu
